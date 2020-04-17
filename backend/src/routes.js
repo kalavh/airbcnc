6d091e8ba1cc7d39a3c8bab40a7085a8caced246
kalavh <jefferson_1996Th@hotmail.com>
2020-04-17 20:08:44 -0300

    session in route

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,7 @@
 const express = require('express')
-
+const SessionController = require('./controlers/SessionControler')
 const routes = express.Router();
 
-routes.get("/", (req,res) => {
-    res.send("arquivo rotas")
-})
+routes.post("/sessions", SessionController.store )
 
 module.exports = routes;
