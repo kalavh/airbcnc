670f26c141fbc6b4c1b2359861f3e5be10bba6c7
kalavh <jefferson_1996Th@hotmail.com>
2020-04-25 00:12:15 -0300

    Add erro

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,10 +1,17 @@
 const Spot = require('../model/Spot')
+const User = require('../model/User')
 module.exports = {
     async store ( req, res ) {
         const {filename} = req.file
         const {company, techs, price} = req.body
         const {user_id} = req.headers
       
+        const user = await User.findById(user_id)
+
+        if (!user) {
+            return res.status(400).json({ Erro : 'Usuario não existe'})
+        }
+
         const spot = await Spot.create({
             user: user_id,
             thumbnail:filename,
