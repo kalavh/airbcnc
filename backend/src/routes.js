6353e1ee13cc768ec9aff14251329c235fecd663
kalavh <jefferson_1996Th@hotmail.com>
2020-04-24 23:42:48 -0300

    upload is working now

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const multer = require('multer')
 const routes = express.Router()
+
 const SessionController = require('./controllers/SessionControler')
 const SpotController = require('./controllers/SpotController')
+const uploadConfig = require('./config/upload')
+
+const upload = multer(uploadConfig)
+
 
 routes.post("/user", SessionController.store)
-routes.post("/spot",SpotController.store)
+routes.post("/spot", upload.single('thumbnail') , SpotController.store)
 
 module.exports = routes;
