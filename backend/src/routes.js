4ab988f24d3d84b6be732cc84edaa80d8bdc9443
kalavh <jefferson_1996Th@hotmail.com>
2020-04-24 23:11:05 -0300

    spot its working

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const routes = express.Router()
 const SessionController = require('./controllers/SessionControler')
+const SpotController = require('./controllers/SpotController')
 
 routes.post("/user", SessionController.store)
+routes.post("/spot",SpotController.store)
 
 module.exports = routes;
