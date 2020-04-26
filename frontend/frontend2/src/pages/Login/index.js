4b4a3756e51bbc2b76444ae20cf94fa263ce5e6c
kalavh <jefferson_1996Th@hotmail.com>
2020-04-26 20:25:59 -0300

    now after login you are in dashboard

--- frontend/frontend2/src/pages/Login/index.js
+++ frontend/frontend2/src/pages/Login/index.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react';
 import api from '../../services/api'
 
-export default function Login() {
+export default function Login({history}) {
+
     const [email,setEmail] = useState('');
-        async function handleSubmit(event) {
+
+    async function handleSubmit(event) {
         // state start with null
         event.preventDefault();
         
@@ -11,6 +13,8 @@ export default function Login() {
         const {_id} = response.data
         
         localStorage.setItem('user',_id)
+
+        history.push('/dashboard')
          }
     return (
         <>
